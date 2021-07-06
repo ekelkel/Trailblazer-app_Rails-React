ff50294e9021db6554a9062a504520764b503a8a
ekelkel <elora.kelkel@gmail.com>
2021-07-06 15:04:17 +0200

    Create static pages controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,2 @@
 class ApplicationController < ActionController::Base
-  def hello
-    render plain: "hello world!"
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  root 'static_pages#home'
+  get 'static_pages/home'
 end
