40cbb1b1a3e95489aa6a470b67ec12bc55c4fb30
ekelkel <elora.kelkel@gmail.com>
2021-07-28 21:46:24 +0200

    Write first system tests using capybara

---
+++ test/system/user_registrations_test.rb
@@ -0,0 +1,38 @@
+require 'application_system_test_case'
+
+class UserRegistrationsTest < ApplicationSystemTestCase
+  test 'visiting home' do
+    visit root_path
+    assert page.has_content? 'Hello World!'
+  end
+
+  test 'invalid signup information' do
+    visit '/signup'
+    fill_in 'Name', with: ''
+    fill_in 'Email', with: 'userinvalid.com'
+    fill_in 'Password', with: 'foo', match: :prefer_exact
+    fill_in 'Password confirmation', with: 'bar', match: :prefer_exact
+    click_button 'Register'
+    assert page.has_content? 'Name required'
+    assert page.has_content? 'Email address is invalid'
+    assert page.has_content? 'Password needs to be 8 characters or more'
+    assert page.has_content? 'Passwords do not match'
+  end
+
+  test 'valid signup information' do
+    # Visit the registration page
+    visit '/signup'
+
+    # Submit the form
+    fill_in 'Name', with: 'elora'
+    fill_in 'Email', with: 'elora@gmail.com'
+    fill_in 'Password', with: 'secretpassword', match: :prefer_exact
+    fill_in 'Password confirmation',
+            with: 'secretpassword',
+            match: :prefer_exact
+    click_button 'Register'
+
+    # Verify that the User was created
+    assert page.has_content? 'We have received your request!'
+  end
+end
